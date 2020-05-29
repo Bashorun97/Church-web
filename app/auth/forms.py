fa2973c2783118a9f8e15828e15e4ef02416b83b
Bashorun97 <bashorun.emma@gmail.com>
2020-05-29 06:01:47 +0100

    Registration working

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,31 +3,40 @@ from wtforms import (
     StringField, SubmitField, IntegerField, PasswordField, BooleanField
 )
 from wtforms.validators import DataRequired, Length, Regexp, EqualTo
+from wtforms import ValidationError
+from models import User
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name')
+    surname = StringField('Surname')
     username = StringField('Username', validators=[
-        DataRequired(), Length(1, 32),
+        DataRequired(), Length(4, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-        'Usersnames must have only letters, numbers, dot or '
-        'underscores')])
+               'Usernames must have only letters, numbers, dots or '
+               'underscores')])
     email = StringField('Email', validators=[DataRequired(), Length(1, 64)])
     #age = IntegerField('Age', validators)
     password  = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    telephone = IntegerField('Telephone number', validators=[
-        DataRequired(), Length(1, 11),
-        Regexp('^[0-9]', 0, 'Telephone should be 11 numbers')])
+    telephone = StringField('Telephone number', validators=[Length(11)])
+    house_address = StringField('Home address', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username or Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    submit = SubmitField('Log In')  
 
 
 class ChangePasswordForm(FlaskForm):
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,60 @@
 from . import auth
+from flask import render_template, redirect, request, url_for, flash, make_response
 from app import db, cors, bcrypt
-from flask import render_template, request, url_for, redirect, flash
+from app.extensions import emailcheck
+from sqlalchemy.exc import IntegrityError
 from flask_login import login_required, login_user, logout_user, current_user
+from .forms import (
+    RegistrationForm, LoginForm, ChangePasswordForm, ResetPasswordForm,
+    ResetPasswordRequestForm
+)
+from models import User, Article
 
-@auth.route('/<name>')
-def hello(name):
-    return render_template('auth/login.html', name=name)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            name = form.name.data,
+            surname = form.surname.data,
+            username = form.username.data,
+            email = form.email.data,
+            password = form.password.data,
+            telephone = form.telephone.data,
+            house_address = form.house_address.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Please check the confirmation email has been sent to you for complete registration')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/registration.html', form=form)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email, username=form.username.data).first() or User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid email or password.')
+    return render_template('/auth/login.html', form=form)
+
+@auth.route('change-password', methods=['GET', 'POST'])
+def change_password():
+    form = ChangePasswordForm()
+    return render_template('/auth/change_password.html', form=form)
+    
+@auth.route('/reset-password', methods=['GET', 'POST'])
+def reset_password():
+    form = ResetPasswordForm()
+    return render_template('/auth/change_password.html', form=form)
+
+@auth.route('/reset-password-request', methods=['GET', 'POST'])
+def reset_password_request():
+    form  = ResetPasswordRequestForm()
+    return render_template('/auth/reset_password.html', form=form)
